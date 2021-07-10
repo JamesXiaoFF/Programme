5fc205653ad14b4fb4479390630cc03eccb5f179
JamesXiaoFF <1341510417@qq.com>
2021-07-11 00:16:14 +0800

    update2

--- LeetCode/09-GreedyAlgorithm/968.监控二叉树.js
+++ LeetCode/09-GreedyAlgorithm/968.监控二叉树.js
@@ -17,8 +17,27 @@
  * @param {TreeNode} root
  * @return {number}
  */
+let res
+var traversal = function (node) {
+    //1->无覆盖，2->有相机，3->有覆盖
+    if (node == null) return 3
+    let left = traversal(node.left)
+    let right = traversal(node.right)
+    if (left == 3 && right == 3) return 1
+    if (left == 1 || right == 1) {
+        res++
+        return 2
+    }
+    if (left == 2 || right == 2) return 3
+}
 var minCameraCover = function (root) {
-
+    res = 0
+    let val = traversal(root)
+    if (val == 1) {
+        res++
+    }
+    return res
 };
+
 // @lc code=end
 
